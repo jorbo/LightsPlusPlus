4c0cab4c43a7ebfaf665db6def50848f19c8dcdd
Jordan Ayvazian <jaayvazian@gmail.com>
2019-07-21 13:23:27 -0700

    added deploy script

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-
-if [[ "$TRAIVS_BRANCH" == "dev" ]]; then
-   /bin/doxygen docs/config
-   /bin/ssh athena "rm -rf /var/www/html/lightsplusplus/*"
-   /bin/scp -r html/* /var/www/html/lightsplusplus/
-fi
+/bin/doxygen docs/config
+/bin/ssh athena "rm -rf /var/www/html/lightsplusplus/*"
+/bin/scp -r html/* /var/www/html/lightsplusplus/
