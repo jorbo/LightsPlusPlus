de73592da8ed5b5f20350f696d56aa1e5145cf01
Jordan Ayvazian <jaayvazian@gmail.com>
2019-08-04 23:21:16 -0700

    removed unused imports from test main

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,12 +2,8 @@
 #include "testing_config.h"
 #include "lights_test.cpp"
 #include "bridge_tests.cpp"
-#define BOOST_NO_CXX11_SCOPED_ENUMS
-#include <boost/filesystem.hpp>
-#undef BOOST_NO_CXX11_SCOPED_ENUMS
-#include "json/writer.h"
-#include <fstream>
-#include <iostream>
+
+
 
 //#define RUN_NETWORK_TESTS //!< Uncomment this line if you want to run network tests (will break travis)
 int main(int argc, char *argv[]) {
